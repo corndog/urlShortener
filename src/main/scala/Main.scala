b6e10f9556acdfaa9a422d3582afe45a3cf3d6ac
Hugh Richardson <hugh.richardson@patch.com>
2013-09-11 20:07:33 -0400

    comments on cake versus with svcTrait

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -6,12 +6,19 @@ import spray.http._
 import spray.http.MediaTypes._
 import com.urlShortener.Services._
 
+// can either just grab one of the services directly by
+// using with WhateverService from ShortenerServices in Main
+// and just call shorten/lengthen directly
+// OR
+// use the service from the component registry, which follows
+// the cake pattern example from Jonas Boner's blog post.
 
-object Main extends App with SimpleRoutingApp {
+object Main extends App with SimpleRoutingApp /*with SqlShortenerService */ {
   implicit val system = ActorSystem("urlShortener")
   val host = "http://127.0.0.1:8080/"
   
-  val service = ComponentRegistry.shortenerService
+  // use this instead of say SqlShortenerService
+  val service = ComponentRegistry.shortenerService 
   
   startServer(interface = "localhost", port = 8080) {
     path("") {
