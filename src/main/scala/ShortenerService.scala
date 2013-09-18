1ad67b8cc0d2ed0861d9e1dc56cdb444f547c703
Hugh Richardson <hugh.richardson@patch.com>
2013-09-17 22:13:20 -0400

    more this.synchronized

--- src/main/scala/ShortenerService.scala
+++ src/main/scala/ShortenerService.scala
@@ -41,28 +41,30 @@ object Services {
     private val password = "password"
     private val user = "postgres"
 
-    def shorten(url:String): Either[java.lang.Throwable, String] = {
+    def _shorten(url:String): Either[java.lang.Throwable, String] = { this.synchronized {
       Database.forURL("jdbc:postgresql:urlShortener",
                     driver = "org.postgresql.Driver",
                     user = user,
                     password = password) withSession {
 
-        if ( ! (UrlValidator.validate(url)) ) {
-	  Left(new java.net.MalformedURLException("Invalid Url"))
-	}
-        else {
-          val shortened = sql"SELECT short_url FROM urls where url = $url".as[String].list.headOption
-          shortened.map( u=> Right(u)).getOrElse( try {
-            val rm = sqlu"INSERT INTO urls (url) VALUES ($url)".first
-	    val id = sql"SELECT id FROM urls where url = $url".as[Long].list.head
-	    val encodedUrl = Base64Encoder.encode(id)
-	    val x = sqlu"UPDATE urls SET short_url = $encodedUrl WHERE url = $url".first
-	    Right(encodedUrl)
-          } catch {
-            case e: java.lang.Throwable => { /*println(e, "hopefully just a constraint violation");*/ Left(e)}
-          })
-        }
+        val shortened = sql"SELECT short_url FROM urls where url = $url".as[String].list.headOption
+        shortened.map( u=> Right(u)).getOrElse( try {
+          val rm = sqlu"INSERT INTO urls (url) VALUES ($url)".first
+	  val id = sql"SELECT id FROM urls where url = $url".as[Long].list.head
+	  val encodedUrl = Base64Encoder.encode(id)
+	  val x = sqlu"UPDATE urls SET short_url = $encodedUrl WHERE url = $url".first
+	  Right(encodedUrl)
+        } catch {
+          case e: java.lang.Throwable => { /*println(e, "hopefully just a constraint violation");*/ Left(e)}
+        })
       }
+    }}
+    
+    def shorten(url:String): Either[java.lang.Throwable, String] = {
+      if ( ! (UrlValidator.validate(url)) )
+	Left(new java.net.MalformedURLException("Invalid Url"))
+      else
+        _shorten(url)
     }
   
     def lengthen(shortUrl:String): Option[String] = {
